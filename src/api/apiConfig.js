5e553188daaf6e4fd33315359a33b22fe8b3e893
mouad aem <mouad.aitelmahjoub@gmail.com>
2021-10-27 11:20:41 +0100

    few tweaks

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,7 @@
+const API_KEY = process.env.React_App_API_KEY
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "3e43264144d009ace9aeea72ef932eda",
+  apiKey: API_KEY,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
